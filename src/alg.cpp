081ab5335e26bdf5a48fc1f45ec231d5f066b1c5
Ivanov Vladimir <99176333+NePridumalUsername@users.noreply.github.com>
2022-06-10 06:31:35 +0700

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,7 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
+#include  <vector>
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    return tree.getPerm(n);
 }
